ce4b0a96c06eeeecadb8c6f14fa084d60619a225
Aasimjazah <asimjazah8@gmail.com>
2023-05-02 23:30:55 +0530

    Initial Commit after creating the SpringBoot Started Project

---
+++ MedicareBackend/src/test/java/com/example/medicare/MedicareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
